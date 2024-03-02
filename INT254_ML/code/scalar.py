46a737a8b03ec25a4986739a153ac6eb1210241c
SanishKumar <18sanishkumar@gmail.com>
2024-03-02 14:15:03 +0530

    first commit

---
+++ INT254_ML/code/scalar.py
@@ -0,0 +1,12 @@
+import numpy as np
+x = np.array([[1],[2],[3]])
+x.shape
+print (f'A 3-D vector:\n{x}\n\n')
+
+y = np.array([[4],[5],[6]])
+print (f'Sum:\n{x+y}\n\n')
+
+
+z = np.array([4,5])
+
+print (x*z)
